2800d18a8e408101361f8435826c474628ccaf33
gregolly <gregollyff@hotmail.com>
2019-03-14 03:06:44 +0000

    mudança na functions.php

--- functions.php
+++ functions.php
@@ -118,16 +118,15 @@ function max_title_length( $title ) {
 //}
 
 // REMOVE A VERSAO DO CSS E JS
-/*
 function vc_remove_wp_ver_css_js( $src ) {
 	if ( strpos( $src, 'ver=' ) )
 	$src = remove_query_arg( 'ver', $src );
 	return $src;
 	}
-	//add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-	//add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-	// fim da remoção da versão css e js
-*/
+	add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+	add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+	 //fim da remoção da versão css e js
+
 	// MOVE O JAVASCRIPT PARA O RODAPE
 function remove_head_scripts() {
 	remove_action('wp_head', 'wp_print_scripts');
