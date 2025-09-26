8c80bf251887cd0ec03d4d7a40fcbe4045abfb5e
gregolly <gregollyff@hotmail.com>
2025-09-26 14:49:29 -0400

    add wp_mail method to send email

---
+++ includes/mail.php
@@ -0,0 +1,18 @@
+<?php
+add_action( 'phpmailer_init', 'configurar_smtp' );
+
+function configurar_smtp( $phpmailer ) {
+    $phpmailer->isSMTP();
+
+    // Configurações do servidor SMTP
+    $phpmailer->Host      = defined('SMTP_HOST') ? SMTP_HOST : '';            // Substitua pelo seu servidor SMTP
+    $phpmailer->SMTPAuth  = true;                                             // Habilita a autenticação SMTP
+    $phpmailer->Port      = defined('SMTP_PORT') ? SMTP_PORT : 587;           // Ou 465 para SSL
+    $phpmailer->Username  = defined('SMTP_USER') ? SMTP_USER : '';            // Seu e-mail ou usuario SMTP
+    $phpmailer->Password  = defined('SMTP_PASS') ? SMTP_PASS : '';            // Sua senha
+
+    // Configurações de criptografia e remetente
+    $phpmailer->SMTPSecure = defined('SMTP_SECURE') ? SMTP_SECURE : 'tls';    // Ou 'ssl' se estiver usando a porta 465
+    $phpmailer->From       = defined('SMTP_FROM') ? SMTP_FROM : '';           // O e-mail que aparecerá como remetente
+    $phpmailer->FromName   = defined('SMTP_NAME') ? SMTP_NAME : '';           // O nome que aparecerá como remetente
+}
