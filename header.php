e10e92321a8ed7ecaaac0c6e3c0314880286c323
gregolly <gregollyff@hotmail.com>
2025-09-29 16:31:05 -0400

    remove function from polylang and add shortcode gtranslate

--- header.php
+++ header.php
@@ -17,22 +17,8 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <div id="top-info-bar" class="... seu código da barra superior ...">
-        </div>
-
-    <div class="menu-de-idiomas-topo">
-        <ul class="d-flex justify-content-center p-2 m-0 gap-3">
-            <?php 
-            // Verifica se a função do Polylang existe antes de chamá-la
-            if ( function_exists( 'pll_the_languages' ) ) {
-                pll_the_languages( array( 
-                    'show_flags' => 1,      // 1 para mostrar bandeiras, 0 para ocultar
-                    'show_names' => 1,      // 1 para mostrar nomes, 0 para ocultar
-                    'dropdown'   => 0       // 1 para criar um dropdown, 0 para lista
-                ) ); 
-            }
-            ?>
-        </ul>
+    <div class="menu-de-idiomas-topo d-flex justify-content-end mr-5 p-2 bg-light">
+        <?= do_shortcode('[gtranslate]'); ?>
     </div>
 
     <header class="site-header sticky-top">
