120a0d333642d92bfa5d73028860e0754a5c175b
gregolly <gregollyff@hotmail.com>
2025-10-28 22:51:33 -0400

    remove title tag from header

--- header.php
+++ header.php
@@ -3,15 +3,10 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-
   <meta charset=”<?php bloginfo('charset'); ?>”>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="description" content="<?php bloginfo('description'); ?>" />
-
-  <title><?php wp_title('|', true,'right'); ?></title>
-
   <?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
